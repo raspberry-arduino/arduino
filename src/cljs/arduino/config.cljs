c0c53c4178e8c4bab0008fa3b456fa66e4380ca7
awb99 <awb99@gmail.com>
2018-12-08 21:17:31 -0500

    client side: hostname resolution dynamically via hostname (c) esteban

--- src/cljs/arduino/config.cljs
+++ src/cljs/arduino/config.cljs
@@ -5,5 +5,28 @@
 
 ; (def base-url "arduino.hoertlehner.com")
 
+(defn get-browser-hostname []
+  (let [hostname js/window.location.hostname]
+    (js/console.log "hostname is: " hostname)
+    hostname
+    ))
 
- (def base-url "localhost:7000")
+; bad ass trick by esteban to not need to change the api endpoints depending on the machine it is running on!!
+ (def base-url
+   (let [browser-hostname (get-browser-hostname)
+         code-hostname "localhost:7000"
+         hostname (if (nil? browser-hostname) code-hostname (str browser-hostname ":7000") )]
+     (js/console.log "react app uses hostname:" hostname)
+     hostname
+     ) )
+
+(comment
+
+  (get-hostname)
+
+  )
+
+
+
+
+
